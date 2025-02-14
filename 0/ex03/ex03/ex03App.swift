51ecb7dc270bae33d7fa667fdec17500411b0924
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2025-02-14 11:52:58 +0500

    ex03

---
+++ 0/ex03/ex03/ex03App.swift
@@ -0,0 +1,17 @@
+//
+//  ex03App.swift
+//  ex03
+//
+//  Created by Mansur Kakushkin on 2/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ex03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
