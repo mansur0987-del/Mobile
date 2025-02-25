c7563f0343b25512f507df508a1526fb8a216f41
mansur0987-del <85795437+mansur0987-del@users.noreply.github.com>
2025-02-26 02:03:21 +0500

    1

--- 4/ex00/diary_app/diary_app/ContentView.swift
+++ 4/ex00/diary_app/diary_app/ContentView.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+import AuthenticationServices
 
 struct ContentView: View {
-	@State var IsLoginButton : Bool = false
+	@State private var isLoading = false
+	private var contextProvider = ContextProvider()
+	
     var body: some View {
         VStack {
 			Text("Welcome to your")
@@ -18,7 +22,7 @@ struct ContentView: View {
 				.font(.system(size: 50))
 				.padding()
 			Button(action: {
-				IsLoginButton.toggle()
+				signInWithWebRedirect()
 			}, label: {
 				Text("Login")
 					.padding()
@@ -28,35 +32,43 @@ struct ContentView: View {
 			.padding()
         }
         .padding()
-		.fullScreenCover(isPresented: $IsLoginButton, content: {
-			LoginSheet(IsLoginButton : $IsLoginButton)
-		})
+//		.fullScreenCover(isPresented: $IsLoginButton, content: {
+//			LoginSheet(IsLoginButton : $IsLoginButton)
+//		})
 		.preferredColorScheme(.dark)
 		.background()
     }
+	func signInWithWebRedirect() {
+		guard let url = URL(string: "https://diary-app-b0f08.web.app/__/auth/handler") else { return }
+		isLoading = true
+		
+		let session = ASWebAuthenticationSession(url: url, callbackURLScheme: "com.googleusercontent.apps.1067011176693-a8t5v428tq6p4e668fsjl9smhs1nuisu", completionHandler: { callbackURL, error in
+			isLoading = false
+			if let error = error {
+				print("Ошибка авторизации: \(error.localizedDescription)")
+				return
+			}
+			print("Успешная авторизация: \(callbackURL?.absoluteString ?? "")")
+		})
+		session.presentationContextProvider = contextProvider // Используем сохраненный объект
+		session.start()
+
+	}
 }
 
 #Preview {
     ContentView()
 }
 
-struct LoginSheet: View {
-	@Binding var IsLoginButton : Bool
-	var body: some View {
-		VStack{
-			HStack{
-				Button(action: {
-					IsLoginButton.toggle()
-				}, label: {
-					Text("Cancel")
-						.padding()
-						.background(.red.tertiary)
-						.cornerRadius(30)
-				})
-				.padding()
-				Spacer()
+class ContextProvider: NSObject, ASWebAuthenticationPresentationContextProviding {
+	func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+			guard let windowScene = UIApplication.shared.connectedScenes
+					.compactMap({ $0 as? UIWindowScene })
+					.first,
+				  let window = windowScene.windows.first else {
+				fatalError("Ошибка: Не удалось найти активное окно")
 			}
-			Spacer()
+			return window
 		}
-	}
 }
+
